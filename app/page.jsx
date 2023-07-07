0926b804d7ba16485c8cc5431087503c2adb0cac
libaice <libaice147@gmail.com>
2023-07-07 15:31:43 +0800

    add nav bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,6 @@
+import { Feed } from "@components/Feed"
+
+
 const Home = () => {
     return (
         <section className="w-full flex-center flex-col" >
@@ -6,6 +9,13 @@ const Home = () => {
                 <br className="max-md:hiden" />
                 <span className="orange_gradient" >AI-Powered Prompts </span>
             </h1>
+            <p className="desc text-center" >
+                Promptopia is an open-source AI Prompt tool for morden world to discover and
+                create and share creative prompts. 
+            </p>
+
+            <Feed />
+
         </section>
     )
 }
